bac8a5c48a2bb8ba6bc90a92032d7a6690b98231
andrew-best-diaxion <abest@diaxion.com>
2017-12-08 13:51:07 +1100

    AzureMonitorHandler now able to detect an insight alert
    \o/

--- AzureMonitorHandler/index.js
+++ AzureMonitorHandler/index.js
@@ -52,13 +52,57 @@ var postMessage = function (message, callback) {
   postReq.end();
 };
 
-// var handleCatchAll = function (event, context) {
+var handleInsightActivity = function (context, req) {
+    var record = req.body || 'Body not found.';
+    var subject = req.body.Subject || 'Subject not found';
+    var timestamp = new Date(req.body.Timestamp).getTime() / 1000 || 'Timestamp not found.';
+    var message = req.body;
+    var color = 'warning';
+
+    if (message.NewStateValue === 'ALARM') {
+      color = 'danger';
+    } else if (message.NewStateValue === 'OK') {
+      color = 'good';
+    }
+
+    // Add all of the values from the event message to the Slack message description
+    var description = '';
+    for (key in message) {
+      var renderedMessage = typeof message[key] === 'object' ?
+        JSON.stringify(message[key]) :
+        message[key];
+
+      description = description + '\n' + key + ': ' + renderedMessage;
+    }
+
+    var slackMessage = {
+      text: '*' + subject + '*',
+      attachments: [{
+        color: color,
+        fields: [{
+            title: 'Message',
+            value: req.body.Name,
+            short: false,
+          },
+          {
+            title: 'Description',
+            value: description,
+            short: false,
+          },
+        ],
+        ts: timestamp,
+      },
+      ],
+    };
+
+    return _.merge(slackMessage, baseSlackMessage);
+  };
+
 var handleCatchAll = function (context, req) {
   var record = req.body || 'Body not found.';
   var subject = req.body.Subject || 'Subject not found';
   var timestamp = new Date(req.body.Timestamp).getTime() / 1000 || 'Timestamp not found.';
-  //var message = JSON.parse(req.body);
-  var message = req.body.Name;
+  var message = req.body;
   var color = 'warning';
 
   if (message.NewStateValue === 'ALARM') {
@@ -101,7 +145,6 @@ var handleCatchAll = function (context, req) {
 };
 
 var processEvent = function (context, req) {
-  //console.log("sns received:" + JSON.stringify(event, null, 2));
   // This is where we determine what sort of message we got from Azure.
   var slackMessage = null;
   // var eventSubscriptionArn = event.Records[0].EventSubscriptionArn;
@@ -131,9 +174,11 @@ var processEvent = function (context, req) {
   //   console.log("processing autoscaling notification");
   //   slackMessage = handleAutoScaling(event, context);
   // }
-  if (false)
+  //if (false)
+  if (req.body.schemaId.indexOf(config.services.insightactivity.match_text) > -1)
   {
-    false;
+    //console.log("processing elasticbeanstalk notification");
+    slackMessage = handleInsightActivity(context, req);
   } else
   {
     slackMessage = handleCatchAll(context, req);
@@ -160,14 +205,12 @@ module.exports = function (context, req) {
   context.log('Function was triggered with data: \n' + JSON.stringify(context, null, 2));
 
   if (hookUrl) {
-    processEvent(event, context);
+    processEvent(context, req);
   } else if (config.unencryptedHookUrl) {
     hookUrl = config.unencryptedHookUrl;
-    // processEvent(event, context);
     processEvent(context, req);
   } else {
     context.log.error('hook url has not been set.');
   }
 
-  context.done();
 };
